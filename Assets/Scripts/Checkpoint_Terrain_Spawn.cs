b2aa6835946f7e0a7c6d30a416ab3b1c7dea7f32
th3j35ter <loganwill921@gmail.com>
2025-04-03 23:28:36 -0500

    Finished desert poly placement
    Completed the full stretch for desert with poly pack.
    Looking into adding the next section before or after the desert section.
    Added scripts to spawn models/terrain at the checkpoint to reduce main file size. This works like a charm and I will implement for each section as well as despawning previous sections.

---
+++ Assets/Scripts/Checkpoint_Terrain_Spawn.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class Checkpoint_Terrain_Spawn : MonoBehaviour
+{
+    public SpawnGroupData_Desert spawnData;
+
+    private bool hasSpawned = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (hasSpawned) return;
+
+        if (other.CompareTag("Player"))
+        {
+            foreach (var info in spawnData.spawnInfos)
+            {
+                Instantiate(info.prefab, info.position, info.rotation);
+            }
+
+            hasSpawned = true;
+            Debug.Log("Spawned group at checkpoint!");
+        }
+    }
+}
